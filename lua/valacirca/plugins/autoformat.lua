7ee645de0ca2efd16790af3f931c53a7b4c2b9c7
Aman Chokshi <aman.chokshi@mcgill.ca>
2026-05-13 19:23:33 -0400

    Not sure what this was about, but seems adding autoformatting support for various languages. Pushing before I make breaking changes

--- lua/valacirca/plugins/autoformat.lua
+++ lua/valacirca/plugins/autoformat.lua
@@ -1,8 +1,7 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
+    cmd = { 'ConformInfo' }, -- Lazy-load the plugin
     keys = {
       {
         '<leader>f',
@@ -15,30 +14,13 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
+      -- Remove 'format_on_save' to prevent autoformatting
+      format_on_save = false,
       formatters_by_ft = {
         lua = { 'stylua' },
         python = {
-          -- To fix auto-fixable lint errors.
           'ruff_fix',
-          -- To run the Ruff formatter.
           'ruff_format',
-          -- To organize the imports.
           'ruff_organize_imports',
         },
         bash = { 'beautysh', 'shellcheck' },
@@ -47,3 +29,52 @@ return {
     },
   },
 }
+-- return {
+--   { -- Autoformat
+--     'stevearc/conform.nvim',
+--     event = { 'BufWritePre' },
+--     cmd = { 'ConformInfo' },
+--     keys = {
+--       {
+--         '<leader>f',
+--         function()
+--           require('conform').format { async = true, lsp_format = 'fallback' }
+--         end,
+--         mode = '',
+--         desc = '[F]ormat buffer',
+--       },
+--     },
+--     opts = {
+--       notify_on_error = false,
+--       format_on_save = function(bufnr)
+--         -- Disable "format_on_save lsp_fallback" for languages that don't
+--         -- have a well standardized coding style. You can add additional
+--         -- languages here or re-enable it for the disabled ones.
+--         local disable_filetypes = { c = true, cpp = true }
+--         local lsp_format_opt
+--         if disable_filetypes[vim.bo[bufnr].filetype] then
+--           lsp_format_opt = 'never'
+--         else
+--           lsp_format_opt = 'fallback'
+--         end
+--         return {
+--           timeout_ms = 500,
+--           lsp_format = lsp_format_opt,
+--         }
+--       end,
+--       formatters_by_ft = {
+--         lua = { 'stylua' },
+--         python = {
+--           -- To fix auto-fixable lint errors.
+--           'ruff_fix',
+--           -- To run the Ruff formatter.
+--           'ruff_format',
+--           -- To organize the imports.
+--           'ruff_organize_imports',
+--         },
+--         bash = { 'beautysh', 'shellcheck' },
+--         latex = { 'latexindent' },
+--       },
+--     },
+--   },
+-- }
